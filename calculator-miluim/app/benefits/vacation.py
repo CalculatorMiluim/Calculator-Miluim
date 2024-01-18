c2d3e0f9ce8ce390d10ac807e9735f8390ae32de
eladb <eladb@netapp.com>
2024-01-18 18:48:30 +0200

    removed dataclass from vacation

--- calculator-miluim/app/benefits/vacation.py
+++ calculator-miluim/app/benefits/vacation.py
@@ -5,13 +5,13 @@ from app.models.consts import CombatLevel
 from app.models.reservist_profile import ReservistProfile
 
 
-@dataclass
 class Vacation(Voucher):
-    name = "שובר חופשה"
-    benefit_type = "voucher"
-    title = "שובר חופשה"
-    description = "שובר חופשה למשרתי מילואים ששירותו לפחות 60 ימי שמ״פ במסגרת מלחמת חרבות ברזל"
-    link_to_source = "https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/"
+    def __init__(self):
+        self.name = "שובר חופשה"
+        self.benefit_type = "voucher"
+        self.title = "שובר חופשה"
+        self.description = "שובר חופשה למשרתי מילואים ששירותו לפחות 60 ימי שמ״פ במסגרת מלחמת חרבות ברזל"
+        self.link_to_source = "https://www.miluim.idf.il/articles-list/%D7%9E%D7%93%D7%99%D7%A0%D7%99%D7%95%D7%AA-%D7%9E%D7%A2%D7%A0%D7%A7%D7%99%D7%9D-%D7%9C%D7%9E%D7%A9%D7%A8%D7%AA%D7%99-%D7%9E%D7%99%D7%9C%D7%95%D7%90%D7%99%D7%9D-%D7%91%D7%9E%D7%9C%D7%97%D7%9E%D7%AA-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/"
 
     def is_eligible(self, reservist: ReservistProfile) -> bool:
         return reservist.calculate_total_days() >= 60
