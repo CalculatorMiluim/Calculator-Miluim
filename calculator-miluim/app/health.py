ad4074c773c0f69264849e7450128f9557730def
Ido <irajuan@netapp.com>
2024-01-18 15:38:38 +0200

    adding staing

--- calculator-miluim/app/health.py
+++ calculator-miluim/app/health.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 health_router = APIRouter()
 
-health_router.get("/check/")
+
+@health_router.get("/check/")
 def health():
     return "ok"
--- calculator-miluim/app/server.py
+++ calculator-miluim/app/server.py
@@ -4,6 +4,6 @@ from app.app import app
 from app.health import health_router
 
 
-app.include_router(health_router, prefix="/health", tags=["Deployment"])
+app.include_router(health_router, prefix="/health")
 
 handler = Mangum(app)
