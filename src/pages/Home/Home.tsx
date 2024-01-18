732b9e4ed7d065cee73347c5db12c0f76d00e967
EfratGutman <efratorden@gmail.com>
2024-01-18 22:43:18 +0200

    add date components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,31 @@
-import React, { useEffect } from 'react'
-import { Box, Typography } from '@mui/material'
-import { useHome } from '@/pages/Home/Home.module.ts'
-import { useAppDispatch } from '@/hooks/reduxHooks.ts'
-import { addInfoMessage, addSuccessMessage } from '@/features/app/appSlice.ts'
+import React from 'react'
+import {Box, Stack, Typography} from '@mui/material'
+import {useHome} from '@/pages/Home/Home.module.ts'
+import {useAppDispatch} from '@/hooks/reduxHooks.ts'
+import DateRange from "@/components/DateRange/DateRange.tsx";
+import {COLORS} from "@/consts/colors.ts";
 
 const Home = () => {
-  const { dummy } = useHome()
+    const {formik} = useHome()
   const dispatch = useAppDispatch()
-  // useEffect(()=>{
-  //     dispatch(addSuccessMessage("sdfasdfdsa"))
-  // },[])
-  return (
-    <Box>
-      <Typography textAlign={'left'} sx={{ mb: 1, fontSize: 25 }}>
-        hello2
-      </Typography>
-    </Box>
-  )
+
+    return (
+        <form onSubmit={formik.handleSubmit} style={{display: 'flex'}}>
+            <Stack sx={{display: 'flex'}}>
+                <Box>
+                    <Typography sx={{color: COLORS.BLACK, fontWeight: 600, textAlign: 'left'}}>
+                        כמות ימי מילואים שביצעת בשנת 2023:
+                    </Typography>
+                    <DateRange/>
+                </Box>
+                <Box>
+                    <Typography sx={{color: COLORS.BLACK, fontWeight: 600, textAlign: 'left'}}>
+                        בתפקיד:
+                    </Typography>
+                </Box>
+            </Stack>
+        </form>
+    );
 }
 
 export default Home
