f3e007fd2487d57e5cab734d11fa554f0019ef5c
eladb <eladb@netapp.com>
2024-01-20 15:19:01 +0200

    benefits added to path

--- calculator-miluim/app/api/benefits.py
+++ calculator-miluim/app/api/benefits.py
@@ -7,7 +7,7 @@ from app.calculator.benefits_calculator import calculate_benefits_for_reservist
 benefits_router = APIRouter()
 
 
-@benefits_router.post("")
+@benefits_router.post("/benefits/")
 def calculate_benefits(reservist: ReservistProfile):
     return calculate_benefits_for_reservist(reservist)
 
