8c38445422959a445e02f312f3aea18c32e9721e
eladb <eladb@netapp.com>
2024-01-20 15:54:16 +0200

    biu and huji added

--- calculator-miluim/app/benefits/grants/haifa_grant.py
+++ calculator-miluim/app/benefits/grants/haifa_grant.py
@@ -8,11 +8,11 @@ class HaifaGrant(Grant):
         super().__init__(
             title="אוניברסיטת חיפה - מענק לימודים",
             description="כל סטודנט וסטודנטית משרתי מילואים בצו 8 יקבלו מלגת עידוד",
-            link_to_source="https://www.bgu.ac.il/standarts/iron-swords/iron-swords-aid-for-students/",
+            link_to_source="https://dekanat.haifa.ac.il/%D7%A2%D7%93%D7%9B%D7%95%D7%A0%D7%99-%D7%93%D7%99%D7%A7%D7%A0%D7%90%D7%98-%D7%94%D7%A1%D7%98%D7%95%D7%93%D7%A0%D7%98%D7%99%D7%9D-%D7%97%D7%A8%D7%91%D7%95%D7%AA-%D7%91%D7%A8%D7%96%D7%9C/",
         )
 
     def is_eligible(self, reservist: ReservistProfile) -> bool:
-        return Academy.BGU == reservist.academy
+        return Academy.HAIFA == reservist.academy
 
     def calculate(self, reservist: ReservistProfile) -> None:
-        self.financial_reward = 1200
+        self.financial_reward = 2000
