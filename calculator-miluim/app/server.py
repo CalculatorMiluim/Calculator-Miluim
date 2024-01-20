b47d933ea60ca37c9ae3d0c7f4bb32fede996e70
eladb <eladb@netapp.com>
2024-01-20 15:09:00 +0200

    import fix

--- calculator-miluim/app/server.py
+++ calculator-miluim/app/server.py
@@ -1,8 +1,8 @@
 from mangum import Mangum
 
 from app.app import app
-from app.health import health_router
 from app.api.benefits import benefits_router
+from app.health import health_router
 
 app.include_router(health_router, prefix="/health")
 app.include_router(benefits_router, prefix="/benefits")
