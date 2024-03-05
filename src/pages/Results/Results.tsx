35e78215a10bcb7e206e92144cfadae939682235
Daniel Benedykt <daniel@benedykt.com>
2024-03-05 16:46:45 -0300

    added print button #29

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Button, Stack, Typography } from '@mui/material'
 import BigNumber from '@/pages/Results/BigNumber/BigNumber.tsx'
 import ToggledListItems from '@/pages/Results/ToggledListItems/ToggledListItems.tsx'
 import { useResults } from '@/pages/Results/Results.module.tsx'
+import PrintIcon from '@mui/icons-material/Print';
 
 const Results = () => {
   const { panelsMap, totalAmount } = useResults()
@@ -11,12 +12,40 @@ const Results = () => {
     window.scrollTo(0, 0)
   }, [])
 
+  const printButton = () => {
+    return (
+      <Button
+                sx={{
+                  mr:10,
+                  ml:10,
+                  minWidth: {
+                    xs: 150,
+                    md: 180,
+                  },
+                }}
+                type="button"
+                variant="outlined"
+                onClick={() => window.print()}
+              >
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '100%' }}>
+                  <Typography sx={{ pb: 0.4, fontFamily: 'PolinBoldFont' }} variant="h5">
+                  הדפסה
+                  </Typography>
+                  <PrintIcon />
+                </Box>
+              </Button>
+    )
+  }
+
   return (
     <Stack alignItems="flex-start" gap={3}>
       <Typography>סכום הכסף שאתה זכאי לו -</Typography>
 
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex' , width: '100%'}}>
+        <Stack direction={"row"} justifyItems={'center'} alignItems={'center'} >
         <BigNumber amount={totalAmount} />
+        {printButton()}
+        </Stack>
       </Box>
 
       <Typography>רשימת הזכויות שמגיעות לך בתור משרת מילואים בתקופת מלחמת חרבות ברזל</Typography>
